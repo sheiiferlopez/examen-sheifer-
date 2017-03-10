55bc415eb40d4293e958e7bf04e7b78942565d46
unknown <estudiante@LAB0237.SERVERINT.LOCAL>
2017-03-10 12:13:06 -0600

    BacoB

---
+++ funsiones.js
@@ -0,0 +1,10 @@
+var BacoB.module=angular.module("BacoB",[]);
+
+BacoB.controller("mostrar",function ($scope) {
+	$scope.Mensaje="iguardia"
+	$scope.Operaciones= ($scope.first)*($scope.second)
+	$scope.first;=10 
+	$scope.second;=5 
+
+});  
+ 
